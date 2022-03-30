36e7c3bbe572377710b99f133791b36e1ba1878e
Carol Vargas <carolmvargas0@gmail.com>
2022-03-30 10:29:23 +0200

    banner home and banner categories

---
+++ src/components/public/Carrousel1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Carrousel1() {
+  return (
+    <div>Carrousel1</div>
+  )
+}
+
+export default Carrousel1;
---
+++ src/components/public/Carrousel2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Carrousel2() {
+  return (
+    <div>Carrousel2</div>
+  )
+}
+
+export default Carrousel2
