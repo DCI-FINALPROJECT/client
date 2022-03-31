a0c391165559edc01fbc2416f7ce9865999e02d9
Felix Hasse <f.hasse@gmail.com>
2022-03-31 09:40:44 +0200

    AdminTemplate

---
+++ src/components/pages/AdminPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import AdminTemplate from '../AdminTemplate'
+import Footer from '../public/Footer'
+import Header from '../public/Header'
+
+function AdminPage() {
+    return (
+        <div>
+            <Header/>
+            <AdminTemplate/>
+            <Footer/>
+        
+        </div>
+    )
+}
+
+export default AdminPage
