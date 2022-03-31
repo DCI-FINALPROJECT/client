412e8e5f0c67a12fe4e9ce9db3053c0090437116
ahmetyanik <ahmetyank4242@gmail.com>
2022-03-31 11:20:26 +0200

    Location of DataStore is changed

--- src/App.js
+++ src/App.js
@@ -2,15 +2,11 @@ import "./App.css";
 import Homepage from "./components/pages/Homepage";
 import { DataStore } from "./DataStore";
 
-
-
 function App() {
   return (
     <div className="App">
       <DataStore.Provider>
-
         <Homepage />
-
       </DataStore.Provider>
     </div>
   );
