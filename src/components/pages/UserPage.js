f920b45c71b68ac3871ee9372e6a46333844ac18
ahmetyanik <ahmetyank4242@gmail.com>
2022-04-16 13:01:15 +0200

    No changing

--- src/components/pages/UserPage.js
+++ src/components/pages/UserPage.js
@@ -20,7 +20,6 @@ function UserPage() {
 
     userPagePermissionControl();
 
-    console.log("USERPERM:",userPermission);
 
 
     return (
--- src/components/public/DeleteTemplate.js
+++ src/components/public/DeleteTemplate.js
@@ -23,19 +23,18 @@ function DeleteTemplate({ product}) {
   return (
     <div className="m-3" style={{ width: "15rem" }}>
       <div className="card card-product-grid mx-2">
+          <Link to={`/product/${product._id}`}>
         <div className="img-wrap">
           <img src={product.images[0]} />
         </div>
+          </Link>
         <div className="info-wrap border-top">
-          <Link to={`/product/${product._id}`}>
             <div className="price-wrap">
               <strong className="price">$ {product.price}</strong>
             </div>
             <div className="" style={{ height: "100px" }}>
-              <h6 className="">{product.productName}</h6>
-              <p className="title mb-2">{product.description}</p>
+              <h6 className="">{product.productNameWithCapacity}</h6>
             </div>
-          </Link>
           <div className="d-flex justify-content-around">
             <button onClick={deleteProduct} className="btn btn-danger">
               Delete
