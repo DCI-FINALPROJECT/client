d5ff76013eb773c1ba758c35eafca7b1656df76d
siddik <spolatde@gmail.com>
2022-04-19 13:49:08 +0200

    product edit function is working

--- src/components/public/AdminNav.js
+++ src/components/public/AdminNav.js
@@ -1,17 +1,21 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 function AdminNav() {
 
-   
+  const path = window.location.pathname
+  const id =useParams().id
+  const prName = useParams().productName
+
   return (
     <div>
       <div class="card p-3" bis_skin_checked="1">
         <nav class="nav nav-pills mx-auto">
-          <a class="nav-link active" aria-current="page" href="/admin">
+          <a class={path==="/admin"?"nav-link active":"nav-link"} aria-current="page" href="/admin">
             Add Product
           </a>
-          <a class="nav-link" href="/admin/deleteproduct/null">
-            Delete Product
+          <a class={path===`/admin/edit/${id}`||path===`/admin/deleteproduct/${prName}`?"nav-link active":"nav-link"} href="/admin/deleteproduct/null">
+           Edit or Delete Product
           </a>
           <a class="nav-link" href="#">
             New Feature
--- src/components/public/EditTemplate.js
+++ src/components/public/EditTemplate.js
@@ -5,42 +5,41 @@ export default function EditTemplate() {
   const params = useParams();
   const id = params.id;
 
-  const [productById, setProductById] = useState();
-
-  const getProductById = async () => {
-    const data =await fetch(`http://localhost:5000/product/${id}`)
-      const response = await data.json();
-      console.log(response);
-      setProductById(response)
-  };
-
-
-  useEffect(()=>{
-    getProductById()
-  },[])
-  console.log("productById", productById);
-
-  const [updateProduct, setUpdateProduct] = useState({
+  const [productById, setProductById] = useState({
     productName: "",
     category: "",
     brand: "",
-    price: 0,
+    price: "",
     description: "",
     images: [],
     quantities: [],
+    capacity: "",
+    stock: { Black: 0, Red: 0, Green: 0, Blue: 0 },
   });
+
+  const getProductById = async () => {
+    const data = await fetch(`http://localhost:5000/product/${id}`);
+    const response = await data.json();
+    setProductById(response);
+  };
+
+  useEffect(() => {
+    getProductById();
+  }, []);
+
+  const [updateProduct, setUpdateProduct] = useState({});
 
   function submitHandler(e) {
     e.preventDefault();
 
-    fetch("http://localhost:5000/admin/updateproduct", {
+    fetch(`http://localhost:5000/admin/updateproduct/${id}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${localStorage.userToken}`,
       },
       body: JSON.stringify(updateProduct),
-    }).then((res) => console.log(res));
+    }).then((res) => res.status===200?alert("product is updated"):console.log(res));
   }
 
   function changeHandle(e) {
@@ -49,7 +48,6 @@ export default function EditTemplate() {
     setUpdateProduct({ ...updateProduct, [e.target.name]: dataByInput });
   }
 
-  console.log("updateProduct", updateProduct);
   return (
     <div>
       <div className="bg">
@@ -57,7 +55,7 @@ export default function EditTemplate() {
           <div className="row">
             <div className="card m-auto " style={{ width: "50%" }}>
               <article className="card-body">
-                <h4 className="mb-4">Add product</h4>
+                <h4 className="mb-4">Edit product</h4>
                 <form
                   action="/admin/updateproduct"
                   onSubmit={submitHandler}
@@ -72,6 +70,7 @@ export default function EditTemplate() {
                         type="text"
                         className="form-control"
                         name="productName"
+                        defaultValue={productById.productName}
                         onChange={changeHandle}
                       />
                     </div>
@@ -83,7 +82,7 @@ export default function EditTemplate() {
                       <input
                         type="text"
                         className="form-control"
-                        placeholder="Type here"
+                        defaultValue={productById.category}
                         name="category"
                         onChange={changeHandle}
                       />
@@ -96,7 +95,7 @@ export default function EditTemplate() {
                       <input
                         type="text"
                         className="form-control"
-                        placeholder="Type here"
+                        defaultValue={productById.brand}
                         name="brand"
                         onChange={changeHandle}
                       />
@@ -109,7 +108,7 @@ export default function EditTemplate() {
                       <input
                         type="text"
                         className="form-control"
-                        placeholder="$ 0.00"
+                        defaultValue={productById.price}
                         name="price"
                         onChange={changeHandle}
                       />
@@ -129,42 +128,6 @@ export default function EditTemplate() {
                         name="images"
                         onChange={changeHandle}
                       />
-                      {/* <div className="gallery-uploader-wrap d-flex justify-content-start">
-                      <div
-                        style={{ width: "80px", height: "80px" }}
-                        className="uploader-img mr-3"
-                      >
-                        <img
-                          width="100%"
-                          src="images/items/1.jpg"
-                        />
-                      </div>
-                      <div
-                        style={{ width: "80px", height: "80px" }}
-                        className="uploader-img mr-3"
-                      >
-                        <img
-                          width="100%"
-                          src="images/items/2.jpg"
-                        />
-                      </div>
-                      <div
-                        style={{ width: "80px", height: "80px" }}
-                        className="uploader-img"
-                      >
-                        <input type="file" name="lorem" />
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="#999"
-                          width="32"
-                          height="32"
-                          viewBox="0 0 24 24"
-                        >
-                          <circle cx="12" cy="12" r="3"></circle>
-                          <path d="M16.83 4L15 2H9L7.17 4H2v16h20V4h-5.17zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5z"></path>
-                        </svg>
-                      </div>
-                    </div> */}
                     </div>
                   </div>
                   {/* description */}
@@ -173,7 +136,7 @@ export default function EditTemplate() {
                     <div className="col-9">
                       <textarea
                         className="form-control"
-                        placeholder="Type here"
+                        defaultValue={productById.description}
                         name="description"
                         onChange={changeHandle}
                       ></textarea>
@@ -181,105 +144,18 @@ export default function EditTemplate() {
                   </div>
                   {/* quantities */}
                   <div className="row mb-4">
-                    <label className="col-3 col-form-label">Quantity</label>
+                    <label className="col-3 col-form-label">Capacity</label>
                     <div className="col-9">
                       <input
                         type="text"
                         className="form-control"
-                        placeholder="Type here"
-                        name="quantities"
+                        defaultValue={productById.capacity}
+                        name="capacity"
                         onChange={changeHandle}
                       />
                     </div>
                   </div>
-                  {/* brand */}
-                  {/* <div className="row mb-4">
-                  <label className="col-3 col-form-label">Brand</label>
-                  <div className="col-9">
-                    <select
-                      className="form-select d-flex justify-content-start"
-                      size="4"
-                    >
-                      <option selected="">Select brand</option>
-                      <option value="1">Asus</option>{" "}
-                      <option value="2">Apple</option>
-                      <option value="3">Xuiaomi </option>
-                      <option value="4">Artel </option>
-                      <option value="6">Google </option>
-                    </select>
-                  </div>
-                </div> */}
-                  {/* features */}
-                  {/*  <div className="row mb-4">
-                  <label className="col-3 col-form-label">Features</label>
-                  <div className="col-9">
-                    <ul className="row row-cols-xxl-3 row-cols-2">
-                      <li>
-                        <label className="form-check">
-                          <input
-                            className="form-check-input"
-                            type="checkbox"
-                            value=""
-                          />
-                          <span className="form-check-label"> Metallic </span>
-                        </label>
-                      </li>
-                      <li>
-                        <label className="form-check">
-                          <input
-                            className="form-check-input"
-                            type="checkbox"
-                            value=""
-                          />
-                          <span className="form-check-label"> 5K Display </span>
-                        </label>
-                      </li>
-                      <li>
-                        <label className="form-check">
-                          <input
-                            className="form-check-input"
-                            type="checkbox"
-                            value=""
-                          />
-                          <span className="form-check-label"> High speed </span>
-                        </label>
-                      </li>
-                      <li>
-                        <label className="form-check">
-                          <input
-                            className="form-check-input"
-                            type="checkbox"
-                            value=""
-                          />
-                          <span className="form-check-label"> Ultra wide </span>
-                        </label>
-                      </li>
-                      <li>
-                        <label className="form-check">
-                          <input
-                            className="form-check-input"
-                            type="checkbox"
-                            value=""
-                          />
-                          <span className="form-check-label"> 5K Display </span>
-                        </label>
-                      </li>
-                      <li>
-                        <label className="form-check">
-                          <input
-                            className="form-check-input"
-                            type="checkbox"
-                            value=""
-                          />
-                          <span className="form-check-label">
-                            {" "}
-                            Blootooth 3.0{" "}
-                          </span>
-                        </label>
-                      </li>
-                    </ul>
-                  </div>
-                </div> */}
+
                   <div className="row mb-2">
                     <div className="col-9 offset-3">
                       <button type="submit" className="btn btn-primary">
