a02bf0cfc69f39a7c6ab13b0f7dd0a5dfef5eebc
siddik <spolatde@gmail.com>
2022-04-27 16:07:21 +0200

    footer updated

--- src/components/public/Footer.js
+++ src/components/public/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div>
+    <div style={{color:"grey"}}>
       <footer className="section-footer border-top">
         <div className="container">
           <section className="footer-top py-3">
@@ -10,34 +10,36 @@ function Footer() {
               <div className="col-md-3">
                 <aside className="">
                   <article className="mr-3">
-                    <img src="../images/logo.svg" className="logo-footer" />
-                    <p className="mt-3">
-                      Some short text about company like You might remember the
-                      Dell computer commercials in which a youth reports this
-                      exciting news to his friends.
-                    </p>
-                    <div>
+                    <a href="/">
+                    <img
+                      src="../images/logo.svg"
+                      className="logo-footer mt-5"
+                    />
+                    </a>
+                    
+
+                    <div className="ml-3 mt-2">
                       <a
-                        className="btn btn-icon btn-light"
+                        className="btn btn-icon btn-light mr-1"
                         title="Facebook"
                         target="_blank"
-                        href="#"
+                        href="https:/facebook.com"
                       >
                         <i className="fab fa-facebook-f"></i>
                       </a>
                       <a
-                        className="btn btn-icon btn-light"
+                        className="btn btn-icon btn-light mr-1"
                         title="Instagram"
                         target="_blank"
-                        href="#"
+                        href="https:/instagram.com"
                       >
                         <i className="fab fa-instagram"></i>
                       </a>
                       <a
-                        className="btn btn-icon btn-light"
+                        className="btn btn-icon btn-light mr-1"
                         title="Youtube"
                         target="_blank"
-                        href="#"
+                        href="https:/youtube.com"
                       >
                         <i className="fab fa-youtube"></i>
                       </a>
@@ -45,7 +47,7 @@ function Footer() {
                         className="btn btn-icon btn-light"
                         title="Twitter"
                         target="_blank"
-                        href="#"
+                        href="https:/twitter.com"
                       >
                         <i className="fab fa-twitter"></i>
                       </a>
@@ -55,69 +57,101 @@ function Footer() {
               </div>
               <div className="col-md-9 row justify-content-around  align-items-center flex-wrap mt-3">
                 <aside className="col-sm-3">
-                  <h6 className="title">About</h6>
+                  <h6 className="title">Contact</h6>
                   <ul className="list-unstyled">
                     <li>
-                      {" "}
-                      <a href="#">About us</a>
+                      <a
+                        className="text-decoration-none "
+                        title="Facebook"
+                        target="_blank"
+                        href="https:/facebook.com"
+                      >
+                        <i className="fab fa-facebook-f mr-1"></i>Facebook
+                      </a>
                     </li>
                     <li>
-                      {" "}
-                      <a href="#">Services</a>
+                      <a
+                        className="text-decoration-none"
+                        title="Instagram"
+                        target="_blank"
+                        href="https:/instagram.com"
+                      >
+                        <i className="fab fa-instagram mr-1"></i>Instagram
+                      </a>
                     </li>
                     <li>
-                      {" "}
-                      <a href="#">Rules and terms</a>
+                      <a
+                        className="text-decoration-none"
+                        title="Youtube"
+                        target="_blank"
+                        href="https:/youtube.com"
+                      >
+                        <i className="fab fa-youtube mr-1"></i>Youtube
+                      </a>
                     </li>
                     <li>
-                      {" "}
-                      <a href="#">Blogs</a>
+                      <a
+                        className="text-decoration-none"
+                        title="Twitter"
+                        target="_blank"
+                        href="https:/twitter.com"
+                      >
+                        <i className="fab fa-twitter mr-1"></i>Twitter
+                      </a>
                     </li>
                   </ul>
                 </aside>
-                <aside className="col-sm-3 ">
-                  <h6 className="title">Services</h6>
+                <aside className="col-sm-3 " style={{color:"grey"}}>
+                  <h6 className="title">Our Team</h6>
                   <ul className="list-unstyled">
                     <li>
-                      {" "}
-                      <a href="#">Help center</a>
+                      <a
+                        href="https://github.com/ahmetyanik"
+                        className=" text-decoration-none"
+                      >
+                        <i className="fab fa-github"></i> Ahmet
+                      </a>
                     </li>
                     <li>
-                      {" "}
-                      <a href="#">Money refund</a>
+                      <a
+                        href="https://github.com/carolmvargas"
+                        className=" text-decoration-none"
+                      >
+                        <i className="fab fa-github"></i> Caroline
+                      </a>
                     </li>
                     <li>
-                      {" "}
-                      <a href="#">Terms and Policy</a>
+                      <a
+                        href="https://github.com/Felix-Hasse"
+                        className=" text-decoration-none"
+                      >
+                        <i className="fab fa-github"></i> Felix
+                      </a>
                     </li>
                     <li>
-                      {" "}
-                      <a href="#">Open dispute</a>
+                      <a
+                        href="https://github.com/s-polat"
+                        className="text-decoration-none"
+                      >
+                        <i className="fab fa-github"></i> Sıddık
+                      </a>
                     </li>
                   </ul>
                 </aside>
-                <aside className="col-sm-3 mt-4 ">
+                <aside className="col-sm-3  ">
                   <h6 className="title">For users</h6>
                   <ul className="list-unstyled">
                     <li>
-                      {" "}
-                      <a href="#"> User Login </a>
+                      <a href="/login"> User Login </a>
                     </li>
                     <li>
-                      {" "}
-                      <a href="#"> User register </a>
+                      <a href="/register"> User register </a>
                     </li>
                     <li>
-                      {" "}
-                      <a href="#"> Account Setting </a>
+                      <a href="/user"> Account Setting </a>
                     </li>
                     <li>
-                      {" "}
-                      <a href="#"> My Orders </a>
-                    </li>
-                    <li>
-                      {" "}
-                      <a href="#"> My Wishlist </a>
+                      <a href="/cartpage"> My Orders </a>
                     </li>
                   </ul>
                 </aside>
@@ -130,10 +164,7 @@ function Footer() {
               <a href="#">Privacy & Cookies</a> &nbsp &nbsp
               <a href="#">Accessibility</a>
             </p>
-            <p className="text-muted">
-              {" "}
-              &copy 2020 Company All rights resetved{" "}
-            </p>
+            <p className="text-muted">&copy 2020 Company All rights resetved</p>
           </section>
         </div>
       </footer>
