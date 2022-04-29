eb0836a93bae772c9facd474b2fec960c248852e
ahmetyanik <ahmetyank4242@gmail.com>
2022-04-29 10:53:00 +0200

    Userpage orderhistory without status completed

--- src/components/public/UserPageOrderHistory.js
+++ src/components/public/UserPageOrderHistory.js
@@ -1,8 +1,58 @@
-import React from 'react'
+import React,{useEffect,useState,useContext} from 'react';
+import { DataStore } from "../../DataStore";
 
 function UserPageOrderHistory() {
+
+  const { user } = useContext(DataStore);
+
+  const [allOrders,setAllOrders] = useState([]);
+
+  const getAllOrders = async () => {
+    await fetch("http://localhost:5000/myallorders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: user.email }),
+    })
+      .then((data) => data.json())
+      .then((data) => setAllOrders(data));
+  };
+
+  useEffect(() => {
+    getAllOrders();
+  }, []);
+
+
+  console.log(allOrders);
+
   return (
-    <div>UserPageOrderHistory</div>
+<table class="table table-striped col-lg-9">
+  <thead>
+    <tr>
+      <th scope="col"></th>
+      <th scope="col">Order Number</th>
+      <th scope="col">Date</th>
+      <th scope="col">Amount</th>
+      <th scope="col">Status</th>
+    </tr>
+  </thead>
+  <tbody>
+{
+  allOrders.map((order,index)=>{
+    return(
+      <tr>
+      <th scope="row">{index+1}</th>
+      <td>{order.orderNumber}</td>
+      <td>{order.date}</td>
+      <td>€ {order.amount}</td>
+      <td> .... </td>
+    </tr>
+    )
+  })
+}
+  </tbody>
+</table>
   )
 }
 
