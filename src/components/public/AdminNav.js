3d09278a266a82f898097cca0f5ada73f25ffe7b
ahmetyanik <ahmetyank4242@gmail.com>
2022-05-09 10:01:02 +0200

    Closing a tag is added

--- src/components/public/AdminNav.js
+++ src/components/public/AdminNav.js
@@ -2,31 +2,57 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 function AdminNav() {
-
-  const path = window.location.pathname
-  const id =useParams().id
-  const prName = useParams().productName
+  const path = window.location.pathname;
+  const id = useParams().id;
+  const prName = useParams().productName;
 
   return (
     <div>
       <div class="card p-3" bis_skin_checked="1">
         <nav class="nav nav-pills mx-auto">
-          <a class={path==="/admin/addproduct"?"nav-link active":"nav-link"} aria-current="page" href="/admin/addproduct">
+          <a
+            class={
+              path === "/admin/addproduct" ? "nav-link active" : "nav-link"
+            }
+            aria-current="page"
+            href="/admin/addproduct"
+          >
             Add Product
           </a>
-          <a class={path===`/admin/edit/${id}`||path===`/admin/deleteproduct/${prName}`?"nav-link active":"nav-link"} href="/admin/deleteproduct/null">
-           Edit or Delete Product
+          <a
+            class={
+              path === `/admin/edit/${id}` ||
+              path === `/admin/deleteproduct/${prName}`
+                ? "nav-link active"
+                : "nav-link"
+            }
+            href="/admin/deleteproduct/null"
+          >
+            Edit or Delete Product
           </a>
-          <a class={path===`/admin/edit/${id}`||path===`/admin/populerproducts`?"nav-link active":"nav-link"} href="/admin/populerproducts">
+          <a
+            class={
+              path === `/admin/edit/${id}` || path === `/admin/populerproducts`
+                ? "nav-link active"
+                : "nav-link"
+            }
+            href="/admin/populerproducts"
+          >
             Populer Products
-          <a class={path===`/admin/couponCreate`?"nav-link active":"nav-link"} href="/admin/couponCreate">
+          </a>
+          <a
+            class={
+              path === `/admin/couponCreate` ? "nav-link active" : "nav-link"
+            }
+            href="/admin/couponCreate"
+          >
             Coupon Creat
           </a>
           <a class="nav-link" href="#">
-          New Feature
+            New Feature
           </a>
           <a class="nav-link" href="#">
-          New Feature
+            New Feature
           </a>
         </nav>
       </div>
