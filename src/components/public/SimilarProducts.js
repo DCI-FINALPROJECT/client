3c9849ab1582e88b5053d87a84185ccc9791dddb
ahmetyanik <ahmetyank4242@gmail.com>
2022-05-18 23:22:42 +0200

    Similar products are not limited

--- src/components/public/SimilarProducts.js
+++ src/components/public/SimilarProducts.js
@@ -1,99 +1,76 @@
-import React,{useState,useEffect,useContext} from 'react'
-import ProductCardCarrousel from './ProductCardCarrousel';
-import Slider from 'react-slick'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
-import { DataStore } from '../../DataStore';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect, useContext } from "react";
+import ProductCardCarrousel from "./ProductCardCarrousel";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { DataStore } from "../../DataStore";
+import { useParams } from "react-router-dom";
 
-function SimilarProducts() {  
-
-  const {productById,setProductById,clicked} = useContext(DataStore);
+function SimilarProducts() {
+  const { productById, setProductById, clicked } = useContext(DataStore);
 
   const params = useParams();
 
   const id = params.id;
 
-  const [selectedCategoryProducts,setSelectedCategoryProducts] = useState([]);
+  const [selectedCategoryProducts, setSelectedCategoryProducts] = useState([]);
 
- 
-  const getProductById = async ()=>{
-
+  const getProductById = async () => {
     await fetch(`http://localhost:5000/product/${id}`)
-    .then((data) => data.json())
-    .then((data) => setProductById(data));
+      .then((data) => data.json())
+      .then((data) => setProductById(data));
+  };
 
-  }
+  const similar = async () => {
+    await fetch(
+      `http://localhost:5000/product/similar/${productById.category}/${id}/${productById.productName}`
+    )
+      .then((data) => data.json())
+      .then((data) => setSelectedCategoryProducts(data));
+  };
 
+  useEffect(() => {
+    getProductById();
+  }, [clicked]);
 
-  const similar = async()=>{
-
-
-    await fetch(`http://localhost:5000/product/similar/${productById.category}/${id}/${productById.productName}`).then(data=>data.json()).then(data=>setSelectedCategoryProducts(data)) 
-  }
-
-  useEffect(()=>{
-    getProductById();  
-  },[clicked])
-
-
-  useEffect(()=>{
+  useEffect(() => {
     similar();
-  },[productById])
+  }, [productById]);
 
-
- 
   const sliderSettings = {
-    dots:true,
+    dots: true,
     slidesToShow: 5,
-    slidesToScroll:5,
+    slidesToScroll: 5,
     infinite: true,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-         slidesToShow: 3,
-         slidesToScroll:3,
-        }
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
       },
       {
         breakpoint: 600,
         settings: {
-         arrows: false,
-         slidesToShow: 1,
-         slidesToScroll:1,
-        }
-       }
-    ]
-  }
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
-  
   return (
-      <div className='container'>
-        <h6 className="home-title"></h6>
+    <div className="container">
+      <h6 className="home-title"></h6>
       <Slider {...sliderSettings}>
-
-      {
-
-        
-        selectedCategoryProducts.map(product=>{
-          return(
-           <ProductCardCarrousel
-           
-           key={product._id}
-           product={product}
-
-            />
-
-          )
-        })
-      }
-        
-       
+        {selectedCategoryProducts.map((product) => {
+          return <ProductCardCarrousel key={product._id} product={product} />;
+        })}
       </Slider>
-        
     </div>
-  )
+  );
 }
 
 export default SimilarProducts;
